587354bc0b636f778503cc0ca074ad38707b088f
ssumthingood <ssumthingood@gmail.com>
2023-07-17 22:46:32 +0900

    design : LogIn, SignUp page

--- Frontend/src/pages/logIn.jsx
+++ Frontend/src/pages/logIn.jsx
@@ -1,6 +1,8 @@
 import Layout from "@/components/Layout";
-import LoginModal from "@/components/LoginModal";
+// import LoginModal from "@/components/LoginModal";
 import useInput from "@/hooks/useInput.js";
+import LoginNav from "@/components/LoginNav.jsx";
+import TextInput from "@/components/TextInput.jsx";
 
 function LogIn() {
   const id = useInput();
@@ -13,7 +15,29 @@ function LogIn() {
   }
   return (
     <Layout>
-      <LoginModal logIn={login} id={id} pw={pw}/>
+      <>
+      {/*<LoginModal logIn={login} id={id} pw={pw}/>*/}
+      {/*<div className="flex-col w-full h-screen top-0 left-0 bottom-0 right-0 justify-center items-center text-center">*/}
+        <LoginNav link="signup" />
+        <div className="relative w-[calc(50rem)] min-h-[calc(30rem)] flex-col rounded-lg  justify-center items-center text-center mx-auto my-72 px-40">
+          <div className="w-full h-10">
+            <span className=" float-left text-lg">로그인</span>
+            <span className=" float-right text-sm text-slate-600">
+            비밀번호를 잊으셨나요?
+          </span>
+          </div>
+          <div className="flex">
+            <TextInput type="text" placeholder="ID" {...id}/>
+          </div>
+          <div className="flex">
+            <TextInput type="password" placeholder="비밀번호" {...pw}/>
+          </div>
+          <button type="button" onClick={login} className=" mt-20">
+            로그인
+          </button>
+        </div>
+      {/*</div>*/}
+        </>
     </Layout>
   );
 }
--- Frontend/src/pages/signUp.jsx
+++ Frontend/src/pages/signUp.jsx
@@ -1,6 +1,8 @@
 import Layout from "@/components/Layout";
-import SignupModal from "@/components/SignupModal";
+// import SignupModal from "@/components/SignupModal";
 import useInput from "@/hooks/useInput.js";
+import LoginNav from "@/components/LoginNav.jsx";
+import TextInput from "@/components/TextInput.jsx";
 
 function SignUp() {
   const id = useInput();
@@ -14,7 +16,50 @@ function SignUp() {
 
   return (
     <Layout>
-      <SignupModal signUp={sign} id={id} pw={pw} pw2={pw2} />
+      <>
+      {/*<SignupModal signUp={sign} id={id} pw={pw} pw2={pw2} />*/}
+      {/*<div className="flex-col w-full h-screen top-0 left-0 bottom-0 right-0 justify-center items-center text-center">*/}
+        <LoginNav link="login" />
+        <div className="relative w-[calc(50rem)] min-h-[calc(30rem)] flex-col rounded-lg  justify-center items-center text-center mx-auto my-72 px-40">
+          <div className="w-full h-10">
+            <span className=" float-left text-lg">회원가입</span>
+          </div>
+          <div className="flex-col">
+            <TextInput
+                type="text"
+                placeholder="ID"
+                {...id}
+            />
+            <div className=" text-red-600 text-sm text-left">
+              불가능한 아이디입니다
+            </div>
+          </div>
+          <div className="flex-col">
+            <TextInput
+                type="password"
+                placeholder="비밀번호"
+                {...pw}
+            />
+            <div className="text-red-600 text-sm text-left">
+              불가능한 비밀번호입니다
+            </div>
+          </div>
+          <div className="flex-col">
+            <TextInput
+                type="password"
+                placeholder="비밀번호확인"
+                {...pw2}
+            />
+            <div className="text-red-600 text-sm text-left">
+              비밀번호가 틀립니다
+            </div>
+          </div>
+          <button type="button" onClick={sign} className=" mt-10">
+            회원가입
+          </button>
+        </div>
+      {/*</div>*/}
+        </>
     </Layout>
   );
 }
