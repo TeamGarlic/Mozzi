9662ae2fb4233fe9385e5757071af7382d9a835c
youn <yyytae0@gmail.com>
2023-07-19 16:51:02 +0900

    feat: clip frame dnd finish

--- Frontend/src/modules/clipAction.js
+++ Frontend/src/modules/clipAction.js
@@ -1,6 +1,10 @@
 export const Clip2Frame = "Clip2Frame";
 export const Frame2Clip = "Frame2Clip";
 export const AddClip = "AddClip";
+export const DragStart = "DragStart";
+export const DragEnd = "DragEnd";
+export const DragClear = "DragClear";
+export const Frame2Frame = "Frame2Frame";
 
 export const Clip2FrameAction = (res) => {
   return {
@@ -16,9 +20,37 @@ export const Frame2ClipAction = (res) => {
   }
 }
 
+export const Frame2FrameAction = (res) => {
+  return {
+    type: Frame2Frame,
+    payload: res
+  }
+}
+
 export const AddClipAction = (res) => {
   return {
     type: AddClip,
+    payload: res
+  }
+}
+
+export const DragStartAction = (res) => {
+  return {
+    type: DragStart,
+    payload: res
+  }
+}
+
+export const DragEndAction = (res) => {
+  return {
+    type: DragEnd,
+    payload: res
+  }
+}
+
+export const DragClearAction = (res) => {
+  return {
+    type: DragClear,
     payload: res
   }
 }
--- Frontend/src/reducers/clipReducer.js
+++ Frontend/src/reducers/clipReducer.js
@@ -1,47 +1,114 @@
-import { Clip2Frame, AddClip, Frame2Clip } from "@/modules/clipAction"
+import { Clip2Frame, AddClip, Frame2Clip, DragStart, DragEnd, DragClear, Frame2Frame } from "@/modules/clipAction"
 
 
 const clipState = {
   frame: {
-    1: {clipIdx: 0, src: ''},
-    2: {clipIdx: 0, src: ''},
-    3: {clipIdx: 0, src: ''},
-    4: {clipIdx: 0, src: ''}},
+    1: {clipIdx: 0, src: ""},
+    2: {clipIdx: 0, src: ""},
+    3: {clipIdx: 0, src: ""},
+    4: {clipIdx: 0, src: ""}},
   clipList: {1: "https://www.kmdb.or.kr/trailer/play/MK041673_P02.mp4"},
+  drag: {
+    start: '',
+    end: '',
+    startIdx: 0,
+    endIdx: 0,
+  }
 }
 
 const clipReducer = (state = clipState, action) => {
   switch(action.type){
     case Clip2Frame: {
-      for (let idx = 1; idx < 5; idx++) {
-        if (!state.frame[idx]['src']){
-          return {
-            ...state,
-            frame: {...state.frame, [idx]: {clipIdx:action.payload.clipIdx, src:action.payload.src}},
-            clipList: {...state.clipList, [action.payload.clipIdx]: ""}
-          }
+      if (!action.payload.frameIdx){
+        for (let idx = 1; idx < 5; idx++) {
+          if (!state.frame[idx]["src"]){
+            return {
+              ...state,
+              frame: {...state.frame, [idx]: {clipIdx:action.payload.clipIdx, src:action.payload.src}},
+              clipList: {...state.clipList, [action.payload.clipIdx]: ""}
+            }}}
+        return {
+          ...state,
         }
-      }
+      } else if(state.frame[action.payload.frameIdx]["src"]){
+        const dummyClip = {
+          clipIdx: state.frame[action.payload.frameIdx]["clipIdx"],
+          src: state.frame[action.payload.frameIdx]["src"]
+        };
+        return {
+          ...state,
+          frame: {...state.frame, [action.payload.frameIdx]: {clipIdx:action.payload.clipIdx, src: action.payload.src}},
+          clipList: {...state.clipList, [dummyClip["clipIdx"]]: dummyClip["src"], [action.payload.clipIdx]: ''}
+        }}
       return {
         ...state,
-      }
+        frame: {...state.frame, [action.payload.frameIdx]: {clipIdx:action.payload.clipIdx, src: action.payload.src}},
+        clipList: {...state.clipList, [action.payload.clipIdx]: ""}
+      };
     }
     case AddClip: {
       return {
         ...state,
         clipList: {...state.clipList, [action.payload.idx]: action.payload.src}
-      }
-    }
+      }}
     case Frame2Clip: {
-      console.log(state.frame)
-      if (state.frame[action.payload.frameIdx]['src']){
+      if (state.frame[action.payload.frameIdx]["src"]){
         return {
           ...state,
-          clipList: {...state.clipList, [state.frame[action.payload.frameIdx]['clipIdx']]: action.payload.src},
+          clipList: {...state.clipList, [state.frame[action.payload.frameIdx]["clipIdx"]]: state.frame[action.payload.frameIdx]["src"]},
           frame: {...state.frame, [action.payload.frameIdx]: {clipIdx:0, src: ""}}
+        }}
+      return state
+    }
+    case Frame2Frame: {
+      console.log(`${state.drag['startIdx']} to ${state.drag['endIdx']}`)
+      const clip = {
+        clipIdx: state.frame[state.drag['startIdx']]['clipIdx'],
+        src: state.frame[state.drag['startIdx']]['src']
+      }
+      if (state.frame[state.drag['endIdx']]['src']){
+        const endClip = {
+          clipIdx: state.frame[state.drag['endIdx']]['clipIdx'],
+          src: state.frame[state.drag['endIdx']]['src']
+        }
+        return {
+          ...state,
+          frame: {...state.frame,
+            [state.drag['startIdx']]:{clipIdx:endClip.clipIdx, src:endClip.src}, [state.drag['endIdx']]:{clipIdx:clip.clipIdx, src:clip.src}}
         }
       }
-      return state
+      return {
+        ...state,
+        frame: {...state.frame, [state.drag['startIdx']]:{clipIdx:0, src:''}, [state.drag['endIdx']]:{clipIdx:clip.clipIdx, src:clip.src}}
+      }
+    }
+    case DragStart: {
+      return {
+        ...state,
+        drag: {
+          ...state.drag,
+          start: action.payload.start,
+          startIdx: action.payload.startIdx,
+        }}}
+    case DragEnd: {
+      return {
+        ...state,
+        drag: {
+          ...state.drag,
+          end: action.payload.end,
+          endIdx: action.payload.endIdx
+        }}}
+    case DragClear: {
+      return {
+        ...state,
+        drag: {
+          ...state.drag,
+          start: '',
+          end: '',
+          startIdx: 0,
+          endIdx: 0
+        }
+      }
     }
 
     default:
