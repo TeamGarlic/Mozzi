4e41c63fcc443905f2b62ff25f25837004be445e
youn <yyytae0@gmail.com>
2023-07-20 09:22:21 +0900

    feat: add dnd functions in aftertake, devide component to frame and cliplist

---
+++ Frontend/src/components/Frame.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Frame2ClipAction, DragEndAction, DragStartAction, Frame2FrameAction } from "@/modules/clipAction";
+
+function Frame(){
+  const [frameNum, setFrameNum] = useState([1, 2, 3, 4]);
+  const frame = useSelector(state => state.clipReducer.frame);
+  const dispatch = useDispatch();
+  const drag = useSelector(state => state.clipReducer.drag);
+
+  function clickVideo(event){
+    dispatch(Frame2ClipAction({frameIdx: event.target.id[5], src: event.target.src}))
+  }
+
+  function onDragStart(event){
+    dispatch(DragStartAction({start:event.target.id.slice(0, 5), startIdx:event.target.id[5]}));
+  }
+
+  function onDragEnd(event){
+    if (drag['end'] === 'clip'){
+      dispatch(Frame2ClipAction({
+        frameIdx: drag['startIdx']
+      }))
+    } else if (drag['end'] === "frame"){
+      dispatch(Frame2FrameAction())
+      console.log(event)
+    }
+  }
+
+  function onDrop(event){
+    console.log(event.target.id.slice(0, 5))
+    dispatch(DragEndAction({end:event.target.id.slice(0, 5), endIdx:event.target.id[5]}))
+  }
+
+  function onDragOver(event){
+    event.preventDefault();
+  }
+
+  function onDragEnter(event){
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      {frameNum.map((i) => {
+        if (frame[i]['src']){
+          return (
+            <div key={`frame${i}`} style={{height: "145px"}} className="pb-3">
+              <video src={frame[i]['src']} width="250" className="pb-3"
+                onClick={clickVideo}
+                onDragStart={onDragStart}
+                onDragEnd={onDragEnd}
+                onDrop={onDrop}
+                onDragOver={onDragOver}
+                onDragEnter={onDragEnter}
+                draggable
+                id={`frame${i}`}>
+              </video>
+            </div>
+          )}
+        return <div key={`frame${i}`} style={{height: "145px"}} className="pb-3"
+                    onDragOver={onDragOver}
+                    onDragEnter={onDragEnter}
+                    onDrop={onDrop}
+                    id={`frame${i}`}
+        ></div>
+      })}
+    </div>
+  )
+}
+
+export default Frame;
