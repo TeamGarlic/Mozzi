c4834d25bb23fe64bc06b97cd73e6fb95d0131d6
ssumthingood <ssumthingood@gmail.com>
2023-07-21 15:20:19 +0900

    feat : booth page

--- Frontend/src/pages/booth.jsx
+++ Frontend/src/pages/booth.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import MakeBooth from "./makeBooth";
 import TakePic from "./takePic";
+import { useParams } from "react-router-dom";
 
 function Booth() {
+  const [taking, setTaking] = useState(false);
+  const { code: sessionID } = useParams();
+  console.log(sessionID);
   return (
     <>
       {/* 여기 두개에다가 아래 canvas 내용을 props로 그대로 넣을거임 */}
-      <MakeBooth />
-      <TakePic />
+      {!taking ? <MakeBooth /> : <TakePic />}
 
       <canvas className=" hidden" />
       <video className=" hidden" />
