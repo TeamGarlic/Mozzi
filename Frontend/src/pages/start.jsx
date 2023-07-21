64f848bf5c88509c67940ceabbd9fcd3e23430c4
ssumthingood <ssumthingood@gmail.com>
2023-07-21 14:30:51 +0900

    feat : timer ing...

--- Frontend/src/pages/start.jsx
+++ Frontend/src/pages/start.jsx
@@ -14,16 +14,16 @@ function Start() {
         <div className="w-[calc(40rem)] flex-col justify-center items-center text-center mx-auto pt-40">
           <div className=" text-6xl pt-40">인 생 클 립</div>
           <div className=" flex justify-center items-center text-center gap-20 overflow-hidden">
-            <div className="mt-20 w-30 h-30 rounded-3xl">
+            <div className="mt-20 w-30 h-30">
               <Link to="/0/makebooth" className="text-center">
-                <MapPinIcon className="w-30 h-30" />
-                <div>부스 만들기</div>
+                <MapPinIcon className="w-fit h-fit" />
+                <div className="bottom-0">부스 만들기</div>
               </Link>
             </div>
-            <div className="mt-20 w-30 h-30 rounded-3xl">
+            <div className="mt-20 w-30 h-30">
               <Link to="/0/makebooth" className="text-center">
-                <UserGroupIcon className="w-30 h-30" />
-                <div>부스 참여하기</div>
+                <UserGroupIcon className="w-fit h-fit" />
+                <div className="bottom-0">부스 참여하기</div>
               </Link>
             </div>
           </div>
--- Frontend/src/pages/takePic.jsx
+++ Frontend/src/pages/takePic.jsx
@@ -4,15 +4,32 @@ import PicSideBar from "../components/PicSideBar";
 import Layout from "../components/Layout";
 import BigCam from "../components/BigCam";
 import Chat from "@/components/Chat";
+import MyRadioGroup from "@/components/MyRadioGroup";
 
 function TakePic() {
   const [taken, setTaken] = new useState(1);
+  const timers = [3, 5, 10];
+  const [timer, setTimer] = useState(3);
+  const [count, setCount] = useState(3);
+  const [timerVisible, setTimerVisible] = useState(false);
+  function timeChange(e) {
+    setTimer(e.target.value);
+    console.log(timer + "로 설정");
+    setCount(e.target.value);
+  }
   function take() {
-    let mytake = taken + 1;
-    if (mytake === 11) {
+    setTimerVisible(true);
+    console.log(timer + "초 후 촬영");
+    const interval = setInterval(() => {
+      setCount((prev) => {
+        return prev - 1;
+      });
+    }, 1000);
+
+    if (taken == 10) {
       location.href = "/0/aftertake";
     } else {
-      setTaken(mytake);
+      setTaken(taken + 1);
     }
   }
   return (
@@ -29,19 +46,21 @@ function TakePic() {
         찰칵
       </Link> */}
         <div className="flex justify-center items-center gap-20 fixed bottom-10 ms-[calc(25%)] w-1/2">
-          <ul className="flex rounded-3xl bg-slate-300 leading-10">
-            <li className=" border-r-2 px-5">타이머</li>
-            <li className=" border-r-2 px-5">3초</li>
-            <li className=" border-r-2 px-5">5초</li>
-            <li className=" px-5">10초</li>
-          </ul>
+          <MyRadioGroup
+            arr={timers}
+            name="timer"
+            onChange={timeChange}
+            nowState={Number(timer)}
+            text={"⏲️"}
+          />
           <div className="flex rounded-2xl bg-yellow-200 leading-10">
             <span className=" px-5 ">{taken}/10</span>
             <button
-              className="w-full leading-10  px-5  bg-red-300 rounded-r-2xl"
+              className={`w-full leading-10  px-5  bg-red-300 rounded-r-2xl`}
               onClick={take}
+              disabled={timerVisible}
             >
-              촬영
+              {timerVisible ? <span>{count}</span> : <span>📷</span>}
             </button>
           </div>
         </div>
