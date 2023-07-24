86a99956fdcf58e3b68b3b66a62898a792e807a6
tngks1995 <tngks1995@naver.com>
2023-07-24 17:47:16 +0900

    feat : 전역적으로 처리된 웹캠 정보를 컴포넌트에 전달

---
+++ Frontend/src/modules/canvasAction.js
@@ -0,0 +1,51 @@
+export const setCamStream = "setCamStream";
+export const setMaskStream = "setMaskStream";
+export const setMainCanvas = "setMainCanvas";
+export const addCamCanvas = "addCamCanvas";
+export const resetCamCanvases = "resetCamCanvases";
+export const setMyLayer = "setMyLayer";
+export const resizeMyLayer = "resizeMyLayer";
+
+export const setCamStreamActaion = (res) => {
+  return {
+    type: setCamStream,
+    payload: res,
+  }
+};
+export const setMaskStreamAction = (res) => {
+  return {
+    type: setMaskStream,
+    payload: res,
+  }
+};
+export const setMainCanvasAction = (res) => {
+  return {
+    type: setMainCanvas,
+    payload: res,
+  }
+};
+export const addCamCanvasAction = (res) => {
+  return {
+    type: addCamCanvas,
+    payload: res,
+  }
+};
+export const resetCamCanvasesAction = (res) => {
+  return {
+    type: resetCamCanvases,
+    payload: res,
+  }
+};
+
+export const setMyLayerAction = (res) => {
+  return {
+    type: setMyLayer,
+    payload: res,
+  }
+};
+export const resizeMyLayerAction = (res) => {
+  return {
+    type: resizeMyLayer,
+    payload: res,
+  }
+};
