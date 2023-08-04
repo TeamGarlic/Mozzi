a157b4a60ff00730dfba772293324c7d647fa930
ssumthingood <ssumthingood@gmail.com>
2023-08-04 12:52:40 +0900

    feat : reIssue with axios InterCeptor : 진짜진짜진짜 -완-

--- Frontend/src/api/boothApi.js
+++ Frontend/src/api/boothApi.js
@@ -9,61 +9,67 @@ const PublicBoothApi = axios.create({
 });
 
 const PrivateBoothApi = axios.create({
-  baseURL: "https://api.mozzi.lol/sessions",
-  config:{
+  baseURL: "https://mozzi.ssafy.life/sessions",
+  config: {
     headers: {
       "Content-Type": "application/json",
-      Authorization : window.localStorage.getItem("accessToken"),
+      Authorization: window.localStorage.getItem("accessToken"),
     },
-  }
+  },
 });
 
-PrivateBoothApi.interceptors.request.use((config)=>{
-  console.log("req interceptor : booth")
-  const token = localStorage.getItem('accessToken');
+PrivateBoothApi.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem("accessToken");
   config.headers.Authorization = token;
   return config;
-  }
-)
+});
 
 PrivateBoothApi.interceptors.response.use(
-  response=>{
+  (response) => {
     return response;
   },
-  async(error)=>{
-    console.log(error)
-    const{
-      config,
-      response:{status},
-    } = error;
+  async (error) => {
+    const { config } = error;
 
-    if(status !== 200){
-      const originRequest = config;
-        try {
-          const tokenResponse = await userApi.reIssue();
-          if (tokenResponse.status === 200) {
-            const newAccessToken = tokenResponse.data.accessToken;
-            localStorage.setItem('accessToken', tokenResponse.data.accessToken);
-            localStorage.setItem(
-              'refreshToken',
-              tokenResponse.data.refreshToken,
-            );
-            axios.defaults.headers.common.Authorization = newAccessToken;
-            originRequest.headers.Authorization = newAccessToken;
-            return axios(originRequest);
-          }
-        } catch (error) {
-          if (axios.isAxiosError(error)) {
-              alert("세션 만료. 다시 로그인해 주세요");
-          }
-        }
+    const originRequest = config;
+    try {
+      const tokenResponse = await userApi.reIssue();
+      if (tokenResponse.status === 200) {
+        const newAccessToken = tokenResponse.data.data.accessToken;
+        localStorage.setItem(
+          "accessToken",
+          tokenResponse.data.data.accessToken
+        );
+        localStorage.setItem(
+          "refreshToken",
+          tokenResponse.data.data.refreshToken
+        );
+        PrivateBoothApi.defaults.headers.common["Authorization"] =
+          newAccessToken;
+        PrivateBoothApi.defaults.headers["Authorization"] = newAccessToken;
+        originRequest.headers["Authorization"] = newAccessToken;
+        let res = await axios(originRequest);
+        return res;
+      } else {
+        alert("세션 만료. 다시 로그인해 주세요");
+        window.localStorage.removeItem("accessToken");
+        window.localStorage.removeItem("refreshToken");
+        window.location.replace("/login");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        alert("세션 만료. 다시 로그인해 주세요");
+        window.localStorage.removeItem("accessToken");
+        window.localStorage.removeItem("refreshToken");
+        window.location.replace("/login");
+      }
     }
   }
-)
+);
 
 const boothApi = {
   createBooth: async () => {
-    let res = await PrivateBoothApi.post("",{});
+    let res = await PrivateBoothApi.post("", {});
     return res;
   },
 
@@ -74,7 +80,7 @@ const boothApi = {
 
   getToken: async (sessionID) => {
     const res = await PublicBoothApi.post(`connections`, {
-      "sessionId" : sessionID,
+      sessionId: sessionID,
     });
     return res;
   },
