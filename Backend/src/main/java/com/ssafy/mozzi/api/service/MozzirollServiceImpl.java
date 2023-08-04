3e66a5015bf3f6084cd9cdad3dba5b036a69436c
minjeong.kim512 <minjeong.kim512@gmail.com>
2023-08-04 14:05:11 +0900

    feat: Add mozziroll list by user #S09P12A109-237

--- Backend/src/main/java/com/ssafy/mozzi/api/service/MozzirollServiceImpl.java
+++ Backend/src/main/java/com/ssafy/mozzi/api/service/MozzirollServiceImpl.java
@@ -1,19 +1,25 @@
 package com.ssafy.mozzi.api.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.mozzi.api.request.MozziLinkPostRequest;
+import com.ssafy.mozzi.api.response.UserMozzirollGetRes;
 import com.ssafy.mozzi.common.exception.handler.AlreadyLinkedMozziException;
 import com.ssafy.mozzi.common.exception.handler.BoothNotExistsException;
 import com.ssafy.mozzi.common.exception.handler.MozzirollNotExists;
 import com.ssafy.mozzi.common.exception.handler.UnAuthorizedException;
 import com.ssafy.mozzi.common.model.response.BaseResponseBody;
 import com.ssafy.mozzi.common.util.MozziUtil;
+import com.ssafy.mozzi.common.util.mapper.MozzirollMapper;
 import com.ssafy.mozzi.db.entity.local.Booth;
 import com.ssafy.mozzi.db.entity.remote.Mozziroll;
+import com.ssafy.mozzi.db.entity.remote.User;
 import com.ssafy.mozzi.db.entity.remote.UserMozziroll;
 import com.ssafy.mozzi.db.repository.local.BoothRepository;
 import com.ssafy.mozzi.db.repository.local.BoothUserRepository;
@@ -83,5 +89,14 @@ public class MozzirollServiceImpl implements MozzirollService {
                     .data(userMozziroll.getId())
                     .build()
             );
+    }
+
+    @Override
+    public UserMozzirollGetRes getMozzirollsByUser(String accessToken, int pageNum, int pageSize) {
+        User user = userService.findUserByToken(accessToken);
+        PageRequest pageRequest = PageRequest.of(pageNum - 1, pageSize);
+        Page<UserMozziroll> page = userMozzirollRepository.findByUserId(user.getId(), pageRequest);
+        List<UserMozziroll> userMozzirolls = page.getContent();
+        return MozzirollMapper.toUserMozzirollGetRes(userMozzirolls, page.getTotalPages());
     }
 }
