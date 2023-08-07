d55a82ab592a238e57aed3b4a87ab0e5f2ecd613
정우정 <wj6349@gmail.com>
2023-08-07 12:57:00 +0900

    hotfix: SpringDoc CORS (set swagger path to https)

--- Backend/src/main/java/com/ssafy/mozzi/MozziApplication.java
+++ Backend/src/main/java/com/ssafy/mozzi/MozziApplication.java
@@ -3,6 +3,10 @@ package com.ssafy.mozzi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://api.mozzi.lol/", description = "Moozi API Server")})
 @SpringBootApplication
 public class MozziApplication {
 
