d6681ff7b8daaad9d392c42b8681f05311571882
정우정 <wj6349@gmail.com>
2023-08-08 10:15:23 +0900

    fix: add swagger server option for local test

--- Backend/src/main/java/com/ssafy/mozzi/MozziApplication.java
+++ Backend/src/main/java/com/ssafy/mozzi/MozziApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://api.mozzi.lol/", description = "Moozi API Server")})
+@OpenAPIDefinition(servers = {@Server(url = "https://api.mozzi.lol/", description = "Mozzi API Server"),
+    @Server(url = "http://localhost:8080/", description = "Local Test")})
 @SpringBootApplication
 public class MozziApplication {
 
