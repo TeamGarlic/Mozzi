aa76ae84b1f23dcdcd6bb797fb461f1fa39ed2f3
tngks1995 <tngks1995@naver.com>
2023-08-10 15:26:27 +0900

    chore : 불필요한 함수 제거

--- Frontend/src/components/BigCam.jsx
+++ Frontend/src/components/BigCam.jsx
@@ -15,18 +15,6 @@ export default function BigCam({myId, updatePosition, setPosition}) {
   const canvasRef = useRef();
   const canvasContextRef = useRef();
 
-
-  const updateLocalPos = (npos) =>{
-    setPosition((prev)=>{
-      const newPosition = [];
-      for(let pos of prev){
-        newPosition.push((pos.id===myId)?npos:pos);
-      }
-      return newPosition;
-    });
-    updatePosition(npos);
-  }
-
   const updateSize = () =>{
     console.log(myId);
     const npos= {
@@ -37,7 +25,13 @@ export default function BigCam({myId, updatePosition, setPosition}) {
       height : rndRef.current.resizable.state.height/(ratio*H),
     };
     dispatch(resizeLayerAction(npos));
-    updateLocalPos(npos);
+    setPosition((prev)=>{
+      const newPosition = [];
+      for(let pos of prev){
+        newPosition.push((pos.id===myId)?npos:pos);
+      }
+      return newPosition;
+    });
     updatePosition(npos);
   }
 
