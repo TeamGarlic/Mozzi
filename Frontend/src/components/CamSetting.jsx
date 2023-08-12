a6ec44bd7ab14c7dafaa7404f24fbc50d3d387a5
tngks1995 <tngks1995@naver.com>
2023-08-12 18:24:51 +0900

    fix : 캠설정을 닫았다 열면 체크박스가 초기화되던 문제 수정

--- Frontend/src/components/CamSetting.jsx
+++ Frontend/src/components/CamSetting.jsx
@@ -43,56 +43,54 @@ function CamSetting() {
 
   return (
     <div className="fixed bottom-5 left-5 z-30">
-      {visible && (
-        <div className="flex-col w-80 h-fit rounded-xl bg-white my-3">
-          <div className="columns-2 relative">
-            <div className=" text-lg p-3">카메라 설정</div>
-            <div className="w-10 h-10 float-right self-center m-2">
-              {camVisibility ? (
-                <EyeIcon
-                  onClick={() => {
-                    setCamVisible(!camVisibility);
-                  }}
-                />
-              ) : (
-                <EyeSlashIcon
-                  onClick={() => {
-                    setCamVisible(!camVisibility);
-                  }}
-                />
-              )}
-            </div>
-          </div>
-          <hr />
-          <div
-            className="overflow-scroll p-3 scrollbar-hide"
-          >
-            <div className="columns-2">
-              <label className="block text-sm font-medium text-gray-900 dark:text-white">카메라 회전 : {`${degreeValue}°`}</label>
-              <div className="flex">
-              <input id="degreeCheckbox" type="checkbox" onChange={(e)=>{setDegreeChecked(e.target.checked)}}
-                     className="w-4 h-4 mr-1 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-              <label htmlFor="degreeCheckbox"
-                     className="block text-sm font-medium text-gray-900 dark:text-white">저는 관종입니다</label>
-              </div>
-            </div>
-            <input type="range" min="0" max="360" value={degreeValue} onChange={(e)=>{setDegree(e.target.value)}}
-                   className="w-full h-2 mb-10 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
-            <hr  className="my-2"/>
-            <div className="columns-2">
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">카메라 크기 : {`${scaleValue}%`}</label>
-              <div className="flex">
-                <input id="scaleCheckbox" type="checkbox" onChange={(e)=>{setScaleChecked(e.target.checked)}}
-                       className="w-4 h-4 mr-1 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                <label htmlFor="scaleCheckbox"
-                       className="block text-sm font-medium text-gray-900 dark:text-white">저는 관종입니다</label>
-              </div>
-            </div>
-            <input type="range" min="30" max="200" value={scaleValue} onChange={(e)=>{setScale(e.target.value)}}
-                   className="w-full h-2 mb-10 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+      <div className={`flex-col w-80 h-fit rounded-xl bg-white my-3 ${visible?"hidden":""}`}>
+        <div className="columns-2 relative">
+          <div className=" text-lg p-3">카메라 설정</div>
+          <div className="w-10 h-10 float-right self-center m-2">
+            {camVisibility ? (
+              <EyeIcon
+                onClick={() => {
+                  setCamVisible(!camVisibility);
+                }}
+              />
+            ) : (
+              <EyeSlashIcon
+                onClick={() => {
+                  setCamVisible(!camVisibility);
+                }}
+              />
+            )}
           </div>
         </div>
-      )}
+        <hr />
+        <div
+          className="overflow-scroll p-3 scrollbar-hide"
+        >
+          <div className="columns-2">
+            <label className="block text-sm font-medium text-gray-900">카메라 회전 : {`${degreeValue}°`}</label>
+            <div className="flex">
+            <input id="degreeCheckbox" type="checkbox" onChange={(e)=>{setDegreeChecked(e.target.checked)}}
+                   className="w-4 h-4 mr-1 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+            <label htmlFor="degreeCheckbox"
+                   className="block text-sm font-medium text-gray-900">저는 관종입니다</label>
+            </div>
+          </div>
+          <input type="range" min="0" max="360" value={degreeValue} onChange={(e)=>{setDegree(e.target.value)}}
+                 className="w-full h-2 mb-10 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+          <hr  className="my-2"/>
+          <div className="columns-2">
+            <label className="block mb-2 text-sm font-medium text-gray-900">카메라 크기 : {`${scaleValue}%`}</label>
+            <div className="flex">
+              <input id="scaleCheckbox" type="checkbox" onChange={(e)=>{setScaleChecked(e.target.checked)}}
+                     className="w-4 h-4 mr-1 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <label htmlFor="scaleCheckbox"
+                     className="block text-sm font-medium text-gray-900">저는 관종입니다</label>
+            </div>
+          </div>
+          <input type="range" min="30" max="200" value={scaleValue} onChange={(e)=>{setScale(e.target.value)}}
+                 className="w-full h-2 mb-10 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+        </div>
+      </div>
       <div className=" w-12 h-12 float-left p-1 rounded-full bg-[#ffffff] border-2 border-blue-500">
         <AdjustmentsHorizontalIcon
           onClick={() => {
