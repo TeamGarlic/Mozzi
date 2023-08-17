7eadc80fb0c5e54fef9bf1fb5f6e8ac31643f508
정우정 <wj6349@gmail.com>
2023-08-17 15:00:27 +0900

    Revert "Merge branch 'be/cors-test' into 'develop-back'"
    This reverts merge request !315

--- Backend/src/main/java/com/ssafy/mozzi/config/CorsConfig.java
+++ Backend/src/main/java/com/ssafy/mozzi/config/CorsConfig.java
@@ -11,34 +11,13 @@ public class CorsConfig implements WebMvcConfigurer {
     @Value("${spring.mozzi.cors}")
     private String[] corsPaths;
 
-    private static final String[] credentialMappings = {
-        "/mozzirolls/**", "/sessions/**", "/files/**", "/items/**",
-        "/users", "/users/reset", "/users/reissue", "/users/register", "/users/logout", "/users/check-login-id"
-    };
-
-    private static final String[] nonCredentialMappings = {
-        "/users/login"
-    };
-
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        for (String mapping : credentialMappings) {
-            registry.addMapping(mapping)
-                .allowedOrigins(corsPaths)
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .maxAge(3000)
-                .allowCredentials(true);
-        }
-
-        for (String mapping : nonCredentialMappings) {
-            registry.addMapping(mapping)
-                .allowedOrigins(corsPaths)
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .maxAge(3000)
-                .allowCredentials(false);
-        }
+        registry.addMapping("/**")
+            .allowedOrigins(corsPaths)
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .maxAge(3000)
+            .allowCredentials(true);
     }
 }
