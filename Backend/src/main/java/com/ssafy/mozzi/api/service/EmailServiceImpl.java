a71e94203051c6fbfa1d1f1e64b8af17c07204af
정우정 <wj6349@gmail.com>
2023-08-19 14:43:16 +0900

    feat: support Third Party SMTPS Service
    (cherry picked from commit 900503c9a44cea39efe757cc5748848f41a9d7f7)

--- Backend/src/main/java/com/ssafy/mozzi/api/service/EmailServiceImpl.java
+++ Backend/src/main/java/com/ssafy/mozzi/api/service/EmailServiceImpl.java
@@ -26,18 +26,24 @@ import jakarta.mail.internet.MimeMessage;
 @PropertySource("classpath:application-keys.properties")
 public class EmailServiceImpl implements EmailService {
 
-    final String senderEmail;
-    final String senderId;
-    final String senderPw;
-    final String smtpDomain;
-    final String smtpHost;
-    final String smtpPort;
-    final String dkimSelector;
-    final DkimSigner dkimSigner;
-    final String dkimClassPath;
+    private final boolean useOwn;
+
+    private final String senderEmail;
+    private final String senderId;
+    private final String senderPw;
+    private final String smtpDomain;
+    private final String smtpHost;
+    private final String smtpPort;
+    private final String dkimSelector;
+    private final DkimSigner dkimSigner;
+    private final String dkimClassPath;
+    private final Authenticator auth;
+    private final String encoding = "utf-8";
 
     @Autowired
     EmailServiceImpl(Environment env) throws Exception {
+        useOwn = "true".equals(env.getProperty("SMTP_OWN"));
+
         senderEmail = env.getProperty("SMTP_SENDER_EMAIL");
         senderId = env.getProperty("SMTP_SENDER_ID");
         senderPw = env.getProperty("SMTP_SENDER_PW");
@@ -47,35 +53,45 @@ public class EmailServiceImpl implements EmailService {
         dkimSelector = env.getProperty("SMTP_DKIM_SELECTOR");
         dkimClassPath = env.getProperty("SMTP_DKIM_CLASSPATH");
 
-        InputStream dkimStream = new ClassPathResource(dkimClassPath).getInputStream();
-        dkimSigner = new DkimSigner(smtpDomain, dkimSelector,
-            dkimStream
-        );
-        dkimSigner.setIdentity(senderEmail);
-        dkimSigner.setHeaderCanonicalization(Canonicalization.SIMPLE);
-        dkimSigner.setBodyCanonicalization(Canonicalization.RELAXED);
-        dkimSigner.setSigningAlgorithm(SigningAlgorithm.SHA256_WITH_RSA);
-        dkimSigner.setLengthParam(true);
-        dkimSigner.setCopyHeaderFields(false);
+        if (useOwn) {
+            InputStream dkimStream = new ClassPathResource(dkimClassPath).getInputStream();
+            dkimSigner = new DkimSigner(smtpDomain, dkimSelector,
+                dkimStream
+            );
+            dkimSigner.setIdentity(senderEmail);
+            dkimSigner.setHeaderCanonicalization(Canonicalization.SIMPLE);
+            dkimSigner.setBodyCanonicalization(Canonicalization.RELAXED);
+            dkimSigner.setSigningAlgorithm(SigningAlgorithm.SHA256_WITH_RSA);
+            dkimSigner.setLengthParam(true);
+            dkimSigner.setCopyHeaderFields(false);
+        } else {
+            dkimSigner = null;
+        }
+
+        auth = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderId, senderPw);
+            }
+        };
     }
 
     @Override
     public void passwordReset(String email, String password) {
-        final String encoding = "utf-8";
+        if (useOwn)
+            useOwn(email, password);
+        else
+            useOracle(email, password);
+    }
 
+    private void useOracle(String email, String password) {
         Properties prop = new Properties();
         prop.put("mail.smtp.host", smtpHost);
         prop.put("mail.smtp.port", Integer.parseInt(smtpPort));
-        prop.put("mail.smtp.auth", "false");
+        prop.put("mail.smtp.starttls.enable", true);
+        prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.quitwait", "false");
-        prop.put("mail.smtp.socketFactory.port", smtpPort);
-        prop.put("mail.smtp.socketFactory.fallback", "false");
-
-        Session session = Session.getDefaultInstance(prop, new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(senderId, senderPw);
-            }
-        });
+        Session session = Session.getDefaultInstance(prop, auth);
 
         MimeMessage message = new MimeMessage(session);
 
@@ -85,7 +101,35 @@ public class EmailServiceImpl implements EmailService {
             message.setSubject("[Mozzi.lol] 패스워드 초기화 안내", encoding);
 
             String content = "초기화된 비밀번호는 " + password + "입니다. 해당 비밀번호를 이용하여 로그인 후 패스워드 변경하시기 바랍니다.";
-            message.setText(content, "utf-8");
+            message.setText(content, encoding);
+            Transport.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void useOwn(String email, String password) {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", smtpHost);
+        prop.put("mail.smtp.port", Integer.parseInt(smtpPort));
+
+
+        prop.put("mail.smtp.auth", "false");
+        prop.put("mail.smtp.quitwait", "false");
+        prop.put("mail.smtp.socketFactory.port", smtpPort);
+        prop.put("mail.smtp.socketFactory.fallback", "false");
+
+        Session session = Session.getDefaultInstance(prop, auth);
+
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(senderEmail));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            message.setSubject("[Mozzi.lol] 패스워드 초기화 안내", encoding);
+
+            String content = "초기화된 비밀번호는 " + password + "입니다. 해당 비밀번호를 이용하여 로그인 후 패스워드 변경하시기 바랍니다.";
+            message.setText(content, encoding);
 
             DkimMessage dkimMessage = new DkimMessage(message, dkimSigner);
             Transport.send(dkimMessage);
